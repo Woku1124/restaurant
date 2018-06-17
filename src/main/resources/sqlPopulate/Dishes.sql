0ddead4b431685d22b29ea615dff58e42568ea75
Morawski-203949 <piter.morawski@wp.pl>
2018-06-18 00:12:05 +0200

    populate of dish tables in db

---
+++ src/main/resources/sqlPopulate/Dishes.sql
@@ -0,0 +1,36 @@
+insert into dish_type (id, name) values ('1','Sniadanie');
+insert into dish_type (id, name) values ('2','Lunch');
+insert into dish_type (id, name) values ('3','Obiad');
+insert into dish_type (id, name) values ('4','Deser');
+insert into dish_type (id, name) values ('5','Podwieczorek');
+insert into dish_type (id, name) values ('6','Kolacja');
+
+
+insert into dish (id, description, name, price, dish_type) values ('10','Potrzebne', 'Platki', '4', '1');
+insert into dish (id, description, name, price, dish_type) values ('11','Potrzebnee', 'Baton', '3', '1');
+insert into dish (id, description, name, price, dish_type) values ('12','Dobre', 'Salatka', '5', '2');
+insert into dish (id, description, name, price, dish_type) values ('13','Dobree', 'Joghurt', '4', '2');
+insert into dish (id, description, name, price, dish_type) values ('14','Smakowite', 'Kurczak', '8', '3');
+insert into dish (id, description, name, price, dish_type) values ('15','Smakowitee', 'Schabowy', '9', '3');
+insert into dish (id, description, name, price, dish_type) values ('16','Pyszne', 'Czekolada', '4', '4');
+insert into dish (id, description, name, price, dish_type) values ('17','Pysznee', 'Banan', '2', '4');
+insert into dish (id, description, name, price, dish_type) values ('18','Zdrowe', 'Musli', '4', '5');
+insert into dish (id, description, name, price, dish_type) values ('19','Zdrowee', 'Shake', '7', '5');
+insert into dish (id, description, name, price, dish_type) values ('20','Niepotrzebne', 'Resztki', '3', '6');
+insert into dish (id, description, name, price, dish_type) values ('21','Niepotrzebnee', 'Soczek', '3', '6');
+
+
+
+insert into rating (id, comment, mark, dish) values ('30','Super', '5', '16');
+insert into rating (id, comment, mark, dish) values ('31','Wow', '4', '16');
+insert into rating (id, comment, mark, dish) values ('32','Great', '5', '16');
+insert into rating (id, comment, mark, dish) values ('33','Best', '5', '16');
+insert into rating (id, comment, mark, dish) values ('34','Bad', '1', '20');
+insert into rating (id, comment, mark, dish) values ('35','Yummy', '2', '20');
+insert into rating (id, comment, mark, dish) values ('36','Disgusting', '1', '20');
+insert into rating (id, comment, mark, dish) values ('37','Ok', '3', '12');
+insert into rating (id, comment, mark, dish) values ('38','Notbad', '3', '12');
+
+
+
+
