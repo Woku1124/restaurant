1a6378609a44b4f4cdadd800387e4920a1d865c8
Morawski-203949 <piter.morawski@wp.pl>
2018-06-17 20:17:21 +0200

    not needed files removal

--- src/main/java/pl/zzpwjj/restaurant/core/repositories/ReservationRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.zzpwjj.restaurant.core.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.zzpwjj.restaurant.core.model.entities.Reservation;
-
-@Repository
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-}
--- src/main/java/pl/zzpwjj/restaurant/core/repositories/TablesRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.zzpwjj.restaurant.core.repositories;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.zzpwjj.restaurant.core.model.entities.Table;
-@Repository
-public interface TablesRepository extends JpaRepository<Table, Long> {
-}
